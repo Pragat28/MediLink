dd269dee4d3b223f2c4852294b860c651a79aa06
shravandeshpandee <147294607+shravandeshpandee@users.noreply.github.com>
2026-05-20 16:54:06 +0530

    Replace nodemailer with Resend for email sending

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,31 +1,13 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  try {
-    console.log("Sending email to:", to);
-    console.log("EMAIL_USER:", process.env.EMAIL_USER);
-    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-    
-    await transporter.sendMail({
-      from: `"Medilink" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text
-    });
-    
-    console.log("Email sent successfully");
-  } catch (err) {
-    console.error("EMAIL ERROR:", err.message);
-    throw err;
-  }
+  await resend.emails.send({
+    from: "Medilink <onboarding@resend.dev>", // use this until you verify a domain
+    to,
+    subject,
+    text
+  });
 };
 
 module.exports = sendEmail;
