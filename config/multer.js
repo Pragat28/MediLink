2fd270df3f39b9d94b057354f8cd0f977cbb686f
shravandeshpandee <147294607+shravandeshpandee@users.noreply.github.com>
2026-05-27 17:06:37 +0530

    Switch multer storage to memory storage
    Replaced Cloudinary storage with memory storage for multer.

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,5 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinary");
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: "medilink",
-      allowed_formats: ["jpg", "jpeg", "png"],
-      public_id: Date.now() + "-" + file.originalname,
-    };
-  },
-});
+const storage = multer.memoryStorage();
 
 module.exports = multer({ storage });
