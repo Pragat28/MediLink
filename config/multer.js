c9d478b0f391164143668f643383f1e898c99255
Pragat28 <pragatibajajj@gmail.com>
2026-05-27 16:37:18 +0530

    Update multer.js

--- config/multer.js
+++ config/multer.js
@@ -3,20 +3,14 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "medilink",
-    allowed_formats: ["jpg", "jpeg", "png"],
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "medilink",
+      allowed_formats: ["jpg", "jpeg", "png"],
+      public_id: Date.now() + "-" + file.originalname,
+    };
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only images allowed"), false);
-  }
-};
-
-module.exports = multer({ storage, fileFilter });
+module.exports = multer({ storage });
